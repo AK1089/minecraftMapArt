c789795dc0f87292d702e9047faa5df22fc74790
Avish K <49350400+AK1089@users.noreply.github.com>
2024-04-19 17:07:36 +0100

    fixed wrong port

--- server.js
+++ server.js
@@ -70,6 +70,6 @@ function formatUUID(id) {
 }
 
 // start the server on port 3000
-app.listen((process.env.PORT || 300), () => {
+app.listen((process.env.PORT || 3000), () => {
     console.log('Server is running on port 3000');
 });
